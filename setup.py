96b180955cdb2344f896d55232e0c09828a27bda
Yulu Jia <yulu.jia@bytedance.com>
2020-09-10 14:23:39 -0700

    ps-lite: update ps-lite
    update ps-lite to use UCX van.
    
    Signed-off-by: Yulu Jia <yulu.jia@bytedance.com>

--- setup.py
+++ setup.py
@@ -867,6 +867,7 @@ class custom_build_ext(build_ext):
                 pass
 
         build_ucx = int(os.environ.get('BYTEPS_WITH_UCX', 0))
+        print("build_ucx is", build_ucx)
         if build_ucx:
             ucx_path = pre_setup.ucx_path.strip()
             if not ucx_path:
